7fa931a0a6a56be2de5a3a03c6cb7dacd6f9bc8f
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-14 22:15:39 +0530

    Create data-types.js

---
+++ 01_basics/data-types.js
@@ -0,0 +1,46 @@
+"use strict" // treat all JS code as newer version
+
+//PRIMITIVE DATA TYPES
+
+let MyName = 'Anant';
+
+let id = 1;
+
+let cars = ['bmw', 'audi'];
+
+let mystatus;
+
+let myWealth = null;
+
+let isLoggedin = false;
+
+let bigNumber = Symbol(123);
+
+// NON PRIMITIVE DATA TYPE
+let user = {
+    email: 'test.com',
+    password: 'test123'
+}
+
+console.log(MyName, id, cars, mystatus, myWealth, isLoggedin, bigNumber, user);
+console.table([typeof MyName, typeof id, typeof cars, typeof mystatus, typeof myWealth, typeof isLoggedin, typeof bigNumber, typeof user])
+
+// INFINITE CURRYING FUNCTION
+
+function add(a) {
+    return function(b) {
+        if(!b) {
+            return a;
+        }
+        return add(a+b);
+    }
+}
+
+console.log(add(100)(200)(300)(400)());
+
+let MyName1 = 'Anant Jadhav';
+
+let findMy = MyName1.length;
+
+console.log(findMy);
+
