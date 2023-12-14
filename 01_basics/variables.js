f881a9cc2cbd09f484015e06ce3a8127d61ee79b
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-14 22:14:28 +0530

    Create variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+// define variables using following keywords
+
+var firstName = 'Anant';
+let lastName = 'Jadhav';
+const id = 1;
+
+// prefer to use let and const
+
+console.table([firstName, lastName, id]);
+
