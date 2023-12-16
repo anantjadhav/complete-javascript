a9f7ffd172faf9fcc32e559ede98aa03136901cb
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-16 23:44:40 +0530

    Create number.js

---
+++ 01_basics/number.js
@@ -0,0 +1,15 @@
+// sum of the positive numbers
+function sumPositiveNumbers(numbers) {
+    let sum = 0;
+    numbers.forEach(function(number){
+        if(number >= 0) {
+            sum = sum + number;
+        }
+    })
+    return sum;
+}
+
+
+// Sample usage - do not modify
+console.log(sumPositiveNumbers([15, -5, 10])); // 25
+console.log(sumPositiveNumbers([-3, 4, -2, 1])); // 5
