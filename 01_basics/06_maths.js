100d89e84cc37d820fc4e635662c59dc27e2378a
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-17 15:34:31 +0530

    Update 06_maths.js

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -21,3 +21,14 @@ const hundreds = 1000000;
 
 // +++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++
 
+/* console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(9.5));
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.9));
+console.log(Math.max(1,2,3,4,5,6));
+console.log(Math.min(4,6,9,8,71,2,3));
+console.log(Math.floor(Math.random()*10) + 1); */
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
