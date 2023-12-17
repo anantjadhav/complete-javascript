8e75f79d4885001e37c1d4fb28adbcff402b1178
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-17 15:15:38 +0530

    new file

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,23 @@
+// NUMBERS AND MATHS IN JAVASCRIPT
+
+let score = 500;
+
+/* console.log(score); */
+
+let balance = new Number(100);
+
+/* console.log(balance); */
+
+/* console.log(balance.toString().length);
+console.log(balance.toFixed(2)); */
+
+const otherNumber = 22.8996;
+
+/* console.log(otherNumber.toPrecision(4)); */
+
+const hundreds = 1000000;
+
+/* console.log(hundreds.toLocaleString("en-IN")); */
+
+// +++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++
+
