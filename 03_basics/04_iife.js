9d367ff9631ed96bdf43013326f8282dab97a06f
Anant Jadhav <anant.jadhav6@gmail.com>
2024-01-02 10:52:31 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -10,3 +10,12 @@
     console.log(`DB Connected two ${name}`);
 } )('anant');
 
+// javascript execution context
+
+// 1. global execution context - this
+// 2. memory phase - alocate memory variables
+//  execution phase (new veriable context)
+
+// functions under the functios 
+
+// LIFO - last in first out
