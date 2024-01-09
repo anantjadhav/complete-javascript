aa7cb35c95586c651d17f3ecb2b9edc90b15e6de
Anant Jadhav <anant.jadhav6@gmail.com>
2024-01-09 10:32:23 +0530

    Update array_loops.js

--- 05_Iterations/array_loops.js
+++ 05_Iterations/array_loops.js
@@ -1,4 +1,4 @@
-// for of loop
+// for in loop
 
 const arr = [1,2,3,4,5,6];
 
@@ -6,3 +6,46 @@ for (const val in arr) {
     /* console.log(val); */
 }
 
+// for of loop
+
+const greeting = 'Hello Anant';
+
+for (const greet of greeting) {
+   /*  console.log(greet); */
+}
+
+// Maps
+
+const map = new Map();
+
+map.set("IN", "INDIA");
+map.set("UK", "UNITED KINGDOM");
+
+/* console.log(map); */
+
+// for in loop
+
+const myObject = {
+    js: "javascript",
+    py: "python",
+    rb: "ruby",
+    cpp: "c++",
+}
+
+for (const key in myObject) {
+   /* console.log(`${key} shortcut is for ${myObject[key]}`); */
+}
+
+const programming = ["js", "py", "ruby", "cpp"];
+
+for (const key in programming) {
+    /* console.log(programming[key]); */
+}
+
+
+// for each loop
+
+
+array.forEach(element => {
+    
+});
